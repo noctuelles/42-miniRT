6d9c21c914068431eff68a02620d20ca53da564c
Benoit Savinel <bsavinel@student.42.fr>
2022-06-14 18:10:33 +0200

    debut intersection plan

---
+++ srcs/plane/intersec_plane.c
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   intersec_plane.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bsavinel <bsavinel@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/06/14 17:17:47 by bsavinel          #+#    #+#             */
+/*   Updated: 2022/06/14 18:10:07 by bsavinel         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minirt_struct.h"
+#include "vector.h"
+
+int ray_intersec_plane_front(double t,t_vec3d normal, t_vec3d ray )
+{
+	if (t < 0 || vec_dot(normal, ray) == 0)
+		return (0);
+	return (0);
+}
+
+t_3dpoint	point_intersection(double t, t_3dpoint eye, t_vec3d ray)
+{
+	return (vec_add(eye, vec_mul_scalar(ray, t)));
+}
+
+double plane_intersection(t_3dpoint point, t_vec3d normal, t_3dpoint eye, t_vec3d ray)
+{
+	double t;
+
+	t = vec_dot(normal, vec_sub(point, eye)) / vec_dot(normal, ray);
+	if (ray_intersec_plane_front(t, normal, ray))
+		return (t);
+}
